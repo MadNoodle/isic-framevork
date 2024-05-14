0b6cc71bb104eb0109509947220f58915adff0cb
Mathieu Janneau <mjanneau@gmail.com>
2024-05-14 14:22:07 +0200

    podspec

---
+++ Isic.xcframework/ios-arm64/Isic.framework/Headers/Isic.h
@@ -0,0 +1,19 @@
+//
+//  Isic.h
+//  Isic Framework
+//
+//  Created by Luc on 14/01/2021.
+//  Copyright © 2021 ISIC. All rights reserved.
+//  Developped by newXt
+//
+
+#import <Foundation/Foundation.h>
+#import <sqlite3.h>
+
+//! Project version number for Isic.
+FOUNDATION_EXPORT double IsicVersionNumber;
+
+//! Project version string for Isic.
+FOUNDATION_EXPORT const unsigned char IsicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Isic/PublicHeader.h>
